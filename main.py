8783d8c6eb21b3a50ee7f23c78f77299b5468cca
Jake Hsu <sieginglion@gmail.com>
2020-07-17 23:47:34 +0800

    add 2002

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import traceback
 import requests as r
 
 markets = {
-    "tw_stock": ["0052.TW", "6533.TW"],
+    "tw_stock": ["0052.TW", "6533.TW", "2002.TW"],
     "us_stock": ["CDNS", "TSM", "SMICY", "AMAT", "TOELY", "LRCX", "KLAC", "INTC", "AMD", "QCOM", "XLNX", "NTDOY", "AAPL", "GOOG", "MSFT", "SNE", "BABA"],
     "crypto": ["BTC-USD", "ETH-USD", "BCH-USD"]
 }
@@ -19,8 +19,13 @@ def main(req):
                 line = line.split(",")
                 if line[4] != "null":
                     closes.append(float(line[4]))
-            drop = 1 - closes[-1] / closes[-2]
-            if drop > 0.025:
-                r.post(bot_api + "/sendMessage", json={"chat_id": 1075192674, "text": f"{symbol} dropped {round(drop * 100, 2)}%"})
+            if symbol == "2002.TW":
+                if closes[-1] >= 22:
+                    r.post(bot_api + "/sendMessage", json={"chat_id": 1075192674, "text": f"{symbol} reached {closes[-1]}%"})
+            else:
+                drop = 1 - closes[-1] / closes[-2]
+                if drop > 0.025:
+                    r.post(bot_api + "/sendMessage", json={"chat_id": 1075192674, "text": f"{symbol} dropped {round(drop * 100, 2)}%"})
         except:
             r.post(bot_api + "/sendMessage", json={"chat_id": 1075192674, "text": traceback.format_exc()})
+
